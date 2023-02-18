b892890692a0aa2725e97f05218e3c83e688b290
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-02-18 15:28:15 -0500

    Implement user reg functionality and correct typo in firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,18 +1,16 @@
-
-import { initializeApp } from "firebase/app";
-import { getFireStore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9y_E8JQk3ATJ4n4_PL-oxJQB3uw3orT4",
-  authDomain: "house-marketplace-app-baac6.firebaseapp.com",
-  projectId: "house-marketplace-app-baac6",
-  storageBucket: "house-marketplace-app-baac6.appspot.com",
-  messagingSenderId: "562566910330",
-  appId: "1:562566910330:web:252811779db17456db06c3"
+  apiKey: 'AIzaSyA9y_E8JQk3ATJ4n4_PL-oxJQB3uw3orT4',
+  authDomain: 'house-marketplace-app-baac6.firebaseapp.com',
+  projectId: 'house-marketplace-app-baac6',
+  storageBucket: 'house-marketplace-app-baac6.appspot.com',
+  messagingSenderId: '562566910330',
+  appId: '1:562566910330:web:252811779db17456db06c3',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const db = getFireStore();
+initializeApp(firebaseConfig);
+export const db = getFirestore();
