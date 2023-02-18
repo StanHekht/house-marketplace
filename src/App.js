746b209e0751a51806a690c1fa72cf351c90fc23
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-02-18 06:15:03 -0500

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import SignIn from './pages/SignIn';
@@ -19,8 +20,8 @@ function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
+      <Navbar />
     </Router>
-    {/* NavBar */}
    </>
   );
 }
