72f0bb082fd169db30500d045576dd1155c5cea2
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-02-19 19:22:24 -0500

    Add functionality to fetch listings from db

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { getAuth, updateProfile } from 'firebase/auth';
 import { doc, updateDoc } from 'firebase/firestore';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { db } from '../firebase.config';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
